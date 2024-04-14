e84f52c6eee99bb05ac6e8c6512502cb3d571c05
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-04-14 20:39:23 +0300

    Add files via upload

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <h3>React documentation site</h3>
+      <p>2024 copyrighting. All right reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './Pages/Header/Header';
+import Main from './Pages/Main/Main';
+import Footer from './Pages/Footer/Footer';
+import Sidebar from './Pages/Sidebar/Sidebar';
+import './global.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div className='container'>
+    <Header />
+    <Main />
+    <Sidebar />
+    <Footer />
+  </div>
+);
+
+
+
